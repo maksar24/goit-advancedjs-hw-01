6b3e25b8dd9b29809db2cd6332a06e4a1d7a8811
maksar24 <74876375+maksar24@users.noreply.github.com>
2024-01-15 22:41:43 +0000

    Deploying to gh-pages from @ maksar24/goit-advancedjs-hw-01@25990ade87a4845eb5323c902b3bd7c4fff3ff61 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as a}from"./assets/vendor-ceec3c52.js";const e=new o("vimeo-player"),t="videoplayer-current-time",n=localStorage.getItem(t),m=function(r){return localStorage.setItem(t,`${r.seconds}`)};e.on("timeupdate",a(m,1e3));e.setCurrentTime(n);
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as a}from"./assets/vendor-ceec3c52.js";const t=new o("vimeo-player"),e="videoplayer-current-time",m=localStorage.getItem(e)??0,n=function({seconds:r}){return localStorage.setItem(e,r)};t.on("timeupdate",a(n,1e3));t.setCurrentTime(m);
 //# sourceMappingURL=commonHelpers2.js.map
